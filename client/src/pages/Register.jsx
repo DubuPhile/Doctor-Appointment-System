43dc447e577eb046fc770b8f10283b86ab3d57df
Charles <enagecharles27@gmail.com>
2025-12-03 02:05:54 +0800

    add REGEX

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,6 +4,9 @@ import { Link, useNavigate } from 'react-router-dom';
 import { message } from 'antd';
 import axios from '../api/axios'
 
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 const Register = () => {
 
   const navigate = useNavigate();
@@ -36,7 +39,14 @@ const Register = () => {
         <Form layout="vertical" onFinish={onFinishHandler}>
           <h3>Register Form</h3>
           <div>
-            <Form.Item label ={<span className="custom-color">Username</span>} name="user">
+            <Form.Item 
+              label ={<span className="custom-color">Username</span>} 
+              name="user"
+              rules={[
+                { required: true, message: 'Enter your username' },
+                { pattern: USER_REGEX, message: 'Username must be 4-24 chars, letters/numbers/-/_ only' }
+              ]}
+            >
               <Input type= "text" required autoComplete='username'/>
             </Form.Item>
             <Form.Item label ={<span className="custom-color">Email</span>} name="email">
@@ -45,7 +55,8 @@ const Register = () => {
             <Form.Item 
               label = {<span className="custom-color">Password</span>} 
               name="password" rules={[
-                {required: true, message: 'Enter your Password'}
+                {required: true, message: 'Enter your Password'},
+                { pattern: PWD_REGEX, message: 'Password must be 8-24 chars, include upper, lower, number & special char (!@#$%)' }
               ]} 
               hasFeedback 
             >
@@ -58,6 +69,7 @@ const Register = () => {
               hasFeedback
               rules={[
                 {required: true, message:'Please confirm your password'},
+                { pattern: PWD_REGEX, message: 'Password must be 8-24 chars, include upper, lower, number & special char (!@#$%)' },
                 ({ getFieldValue }) => ({
                   validator(_, value) {
                     if (!value || getFieldValue('password') === value) {
