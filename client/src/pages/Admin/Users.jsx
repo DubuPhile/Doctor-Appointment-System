313cb437353919e5d407b90e3995f3f13ab06150
Charles <enagecharles27@gmail.com>
2025-12-19 00:24:33 +0800

    Add user page

---
+++ client/src/pages/Admin/Users.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react"
+import Layout from "../../components/Layout"
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import useAuth from "../../hooks/useAuth";
+import { Table } from "antd";
+
+const Users = () => {
+  const { auth } = useAuth();
+  const axiosPrivate = useAxiosPrivate();
+  const [ users, setUsers ] = useState([]);
+
+  useEffect(() =>{
+    let isMounted = true
+
+    if(!auth?.accessToken) return;
+    const getUsers = async() => {
+      
+      try{
+        const res = await axiosPrivate.get('/admin/getAllUsers')
+        if(isMounted && res.data?.success){
+          setUsers(res.data.data);
+        }
+      } catch(err){
+        console.log(err)
+      }
+    }
+    
+    getUsers();
+
+    return () => {
+      isMounted = false
+    }
+  },[auth?.accessToken, axiosPrivate])
+
+  //ant table column
+  const columns = [
+    {
+      title: 'Users',
+      dataIndex: 'user',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'Roles',
+      dataIndex: 'roles',
+      render: ( text , record ) => (
+        <span>{ Object.values(record.roles || {}).includes(5150) ? 'Admin' : 'User' }</span>
+      )
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      render: ( text , record ) => (
+        <div className="d-flex">
+          <button className="btn btn-danger">Block</button>
+        </div>
+      )
+    },
+  ]
+
+  return (
+    <Layout>
+        <h1 className="text-center">Users Lists</h1>
+        <br/>
+        <Table columns={columns} dataSource={users} rowKey={'_id'}/>
+    </Layout>
+  )
+}
+
+export default Users
