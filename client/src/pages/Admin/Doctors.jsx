c071dd527654f88192029d5b05409848ab5d2b22
Charles <enagecharles27@gmail.com>
2025-12-20 21:43:35 +0800

    add handleAccountStatus

--- client/src/pages/Admin/Doctors.jsx
+++ client/src/pages/Admin/Doctors.jsx
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout"
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import useAuth from "../../hooks/useAuth";
 import { Table } from "antd";
+import Spinner from "../../components/Spinner";
 
 
 const Doctors = () => {
@@ -10,20 +11,21 @@ const Doctors = () => {
   const { auth } = useAuth();
   const axiosPrivate = useAxiosPrivate();
   const [ doctors, setDoctors ] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() =>{
     let isMounted = true
-
     if(!auth?.accessToken) return;
     const getDoctors = async() => {
-      
-      try{
+      try{   
         const res = await axiosPrivate.get('/admin/getAllDoctors')
         if(isMounted && res.data?.success){
           setDoctors(res.data.data);
         }
       } catch(err){
         console.log(err)
+      } finally{
+        if(isMounted) setIsLoading(false)
       }
     }
     
@@ -32,8 +34,25 @@ const Doctors = () => {
     return () => {
       isMounted = false
     }
-  },[auth?.accessToken, axiosPrivate])
+  },[axiosPrivate, auth?.accessToken, isLoading])
 
+  
+  const handleAccountStatus = async( record, status ) => {
+    setIsLoading(true)
+    try{
+      const res = await axiosPrivate.post('/admin/changeAccountStatus', {doctorsId: record._id, status: status})
+  
+      if(res.data.success){
+        message.success(res.data.message);
+      }
+    } catch(err){
+      console.log(err)
+      message.error('Something went Wrong!')
+    } finally{
+      setIsLoading(false)
+    }
+  }
+  
   const columns = [
     {
       title: 'Users',
@@ -56,7 +75,7 @@ const Doctors = () => {
       render: (text, record) => (
         <div className="d-flex">
           {record.status === 'pending' 
-            ? <button className="btn btn-success">Approve</button> 
+            ? <button className="btn btn-success" onClick={() => handleAccountStatus(record, 'approved')}>Approve</button> 
             : <button className="btn btn-danger">Reject</button>}
         </div>
       )
@@ -65,9 +84,13 @@ const Doctors = () => {
 
   return (
     <Layout>
-        <h1 className="text-center">All Doctors</h1>
-        <br/>
-        <Table columns={columns} dataSource={doctors} rowKey={'_id'}/>
+        {isLoading ? (<Spinner />) : (
+        <>
+          <h1 className="text-center">All Doctors</h1>
+          <br/>
+          <Table columns={columns} dataSource={doctors} rowKey={record => record._id}/>
+        </>
+        )}
     </Layout>
   )
 }
