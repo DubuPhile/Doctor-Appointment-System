e42d3db676dcaf404d754e55e5dda8383cd5f07e
Charles <enagecharles27@gmail.com>
2025-12-23 02:09:50 +0800

    add update Controller

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,5 +1,6 @@
 const doctorsModel = require('../models/doctorsModels')
 
+//get doctor Info
 const getDoctorInfoController = async( req, res ) => {
     const {userId} = req.body
     try{
@@ -18,5 +19,24 @@ const getDoctorInfoController = async( req, res ) => {
         })
     }
 }
+//update doctor Profile
+const updateProfileController = async( req, res ) => {
+    const {userId} = req.body
+    try{
+        const doctor = await doctorsModel.findOneAndUpdate({ userId }, req.body)
+        res.status(201).send({
+            success:true,
+            message: 'Doctor Profile Updated',
+            data: doctor,
+        })
+    }catch(err){
+        console.log(err)
+        res.status(500).send({
+            success: false,
+            err,
+            message: "Invalid Update"
+        })
+    }
+}
 
-module.exports = { getDoctorInfoController }
+module.exports = { getDoctorInfoController, updateProfileController }
