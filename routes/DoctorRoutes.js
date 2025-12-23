88b0bc4488b58b50abdf16a910558810534f3fdd
Charles <enagecharles27@gmail.com>
2025-12-24 00:12:59 +0800

    add update Profile router

--- routes/DoctorRoutes.js
+++ routes/DoctorRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getDoctorInfoController} = require ('../controllers/doctorController');
+const {getDoctorInfoController, updateProfileController } = require ('../controllers/doctorController');
 
 //get all users
 router.post('/getDoctorInfo', getDoctorInfoController);
+
+router.post('/updateProfile', updateProfileController);
 
 module.exports = router
