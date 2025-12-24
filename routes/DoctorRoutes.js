88a673621e9c0a3fdd4c3c0427ebe48686411904
Charles <enagecharles27@gmail.com>
2025-12-25 02:37:54 +0800

    add getDoctorId routes

--- routes/DoctorRoutes.js
+++ routes/DoctorRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getDoctorInfoController, updateProfileController } = require ('../controllers/doctorController');
+const {getDoctorInfoController, updateProfileController, getDoctorIdController } = require ('../controllers/doctorController');
 
 //get all users
 router.post('/getDoctorInfo', getDoctorInfoController);
 
 router.post('/updateProfile', updateProfileController);
+
+router.get('/getDoctorId', getDoctorIdController);
 
 module.exports = router
