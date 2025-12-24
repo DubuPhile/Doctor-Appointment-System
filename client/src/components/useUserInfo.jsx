f7e1cbabd190c4a33ea76923b539b007da63cd65
Charles <enagecharles27@gmail.com>
2025-12-25 02:40:14 +0800

    add userInfo decoded

--- client/src/components/useUserInfo.jsx
+++ client/src/components/useUserInfo.jsx
@@ -10,6 +10,7 @@ export const useUserInfo = () => {
 
     const decoded = jwtDecode(auth.accessToken);
     return {
+      userInfo:decoded?.UserInfo || null,
       userId:decoded?.UserInfo?._id || decoded?._id || null,
       user: decoded?.UserInfo?.user || decoded?.user || null,
       isDoctor: decoded?.UserInfo?.isDoctor || decoded?.isDoctor || null,
