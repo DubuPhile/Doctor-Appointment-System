f89b60dc85171624e4aa44e07bf3504c39954b76
Charles <enagecharles27@gmail.com>
2025-12-26 00:08:03 +0800

    appointment schema

---
+++ models/appointmentModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema;
+
+const appointmentSchema = new schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    doctorId: {
+        type: String,
+        required: true,
+    },
+    doctorInfo: {
+        type: String,
+        required: true,
+    },
+    userInfo: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        default: "pending"
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+},{timestamps:true})
+module.exports = mongoose.model('Appointment', appointmentSchema);
