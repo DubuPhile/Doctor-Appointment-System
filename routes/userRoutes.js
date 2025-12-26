3f4ddaf999d736dc1ec07f5cbcdf7041cdd24fac
Charles <enagecharles27@gmail.com>
2025-12-27 05:17:27 +0800

    add the routes of availability

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getApprovedDoctorController, bookAppointmentController } = require('../controllers/userController')
+const { getApprovedDoctorController, bookAppointmentController, bookAvailabilityController } = require('../controllers/userController')
 
 //get Approved Doctors
 router.get('/getApprovedDoctors', getApprovedDoctorController);
 
 //Book Appointment
 router.post('/book-appointment', bookAppointmentController);
+// book available
+router.post('/book-availability', bookAvailabilityController);
 
 module.exports = router
