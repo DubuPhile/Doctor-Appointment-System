bc64ffa334da2c5854cb900ec67791c52a229e44
Charles <enagecharles27@gmail.com>
2025-12-31 00:43:00 +0800

    update status route

--- routes/DoctorRoutes.js
+++ routes/DoctorRoutes.js
@@ -5,14 +5,18 @@ const {
     updateProfileController, 
     getDoctorIdController, 
     getDoctorAppointmentsController,
+    updateStatusController,
 } = require ('../controllers/doctorController');
 
 //get all users
 router.post('/getDoctorInfo', getDoctorInfoController);
-
+//Update Profile
 router.post('/updateProfile', updateProfileController);
-
+//get Doctor ID
 router.get('/getDoctorId', getDoctorIdController);
-
+//get Doctor appointments
 router.get('/doctor-appointments', getDoctorAppointmentsController)
+router.get('/getDoctorId', getDoctorIdController);
+//update status
+router.post('/update-status', updateStatusController)
 module.exports = router
