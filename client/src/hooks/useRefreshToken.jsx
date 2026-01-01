191c3ab6f7931907c1ebd9bb0eaaa814939e0579
Charles <enagecharles27@gmail.com>
2026-01-02 01:33:36 +0800

    remove inline credentials

--- client/src/hooks/useRefreshToken.jsx
+++ client/src/hooks/useRefreshToken.jsx
@@ -5,7 +5,6 @@ const useRefreshToken = () => {
     const {setAuth} = useAuth();
     const refresh = async () => {
         const response = await axios.get('/refresh', {
-            withCredentials: true
         });
         setAuth(prev => {
             // console.log(JSON.stringify(prev));
