47ccf755244537edf1f5bdec64f06e30fc66d622
Charles <enagecharles27@gmail.com>
2026-01-01 23:49:01 +0800

    fixed

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,7 +2,7 @@ const allowedOrigin = [
     'http://localhost:3500',
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://doctor-appointment-mu-lemon.vercel.app/'
+    'https://doctor-appointment-mu-lemon.vercel.app'
 ];
 
 module.exports = allowedOrigin
