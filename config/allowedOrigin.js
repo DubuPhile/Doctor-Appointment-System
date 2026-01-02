ad27905f76ffef60101055d25fc76adec5620bab
Charles <enagecharles27@gmail.com>
2026-01-02 21:48:13 +0800

    add backend page

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,7 +2,8 @@ const allowedOrigin = [
     'http://localhost:3500',
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://doctor-appointment-mu-lemon.vercel.app'
+    'https://doctor-appointment-mu-lemon.vercel.app',
+    'https://doctor-appointment-dkp7.onrender.com'
 ];
 
 module.exports = allowedOrigin
