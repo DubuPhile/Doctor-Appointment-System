09f4c2355801cfe2cc9be09d81fda77d7a794ce2
Charles <enagecharles27@gmail.com>
2026-01-07 01:07:27 +0800

    add path in res.cookie

--- controllers/googleAuthController.js
+++ controllers/googleAuthController.js
@@ -67,7 +67,8 @@ const googleLogin = async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: 'None',
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            path: '/',
         });
 
         res.json({ accessToken });
