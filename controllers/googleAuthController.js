fa2bc6139721dc5096b09f8cc03189eeaaea3e1a
Charles <enagecharles27@gmail.com>
2026-01-07 00:18:24 +0800

    googleAuth Controller

---
+++ controllers/googleAuthController.js
@@ -0,0 +1,81 @@
+const { OAuth2Client } = require('google-auth-library');
+const User = require('../models/userModels');
+const jwt = require('jsonwebtoken');
+
+
+
+const googleLogin = async (req, res) => {
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    const { token } = req.body;
+    console.log(req.body)
+    try {
+        // 1. Verify token with Google
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        });
+
+        const { email, name, sub: googleId } = ticket.getPayload();
+
+        // 2. Find user
+        let user = await User.findOne({ email }).select('+googleId');
+
+        // 3. Create user if not exists
+        if (!user) {
+            user = await User.create({
+                user: name,
+                email,
+                googleId,
+                authProvider: 'google',
+            });
+        }
+
+        // 4. Link Google if registered normally before
+        if (!user.googleId) {
+            user.googleId = googleId;
+            user.authProvider = 'google';
+            await user.save();
+        }
+
+        // 5. Create JWTs
+        const roles = Object.values(user.roles).filter(Boolean);
+        const accessToken = jwt.sign(
+            {
+                "UserInfo":{
+                        "_id": user._id,
+                        "user": user.user,
+                        "isDoctor": user.isDoctor,
+                        "roles": roles
+                    }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '15m' }
+        );
+
+        const refreshToken = jwt.sign(
+            { userId: user._id },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '7d' }
+        );
+
+        // 6. Save refresh token
+        user.refreshToken = refreshToken;
+        await user.save();
+
+        // 7. Send cookie
+        res.cookie('jwt', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        res.json({ accessToken });
+
+    } catch (err) {
+        console.error(err);
+        res.status(401).json({ message: 'Google authentication failed' });
+    }
+};
+
+module.exports = { googleLogin };
