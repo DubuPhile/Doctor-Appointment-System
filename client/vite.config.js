fb221526803021152633f30b41b5a55fdf645a70
Charles <enagecharles27@gmail.com>
2026-01-07 00:55:57 +0800

    cross-origin unsafe-none

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
+    },
+  },
 })
