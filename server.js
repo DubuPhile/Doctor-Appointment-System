cea05256d1534ef8be6ddd5187d824bf2a682b69
Charles <enagecharles27@gmail.com>
2026-01-08 00:28:28 +0800

    rename to firebaseAuth

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/register', require('./routes/register'));
 app.use('/login', require('./routes/login'));
 app.use('/refresh', require('./routes/refreshToken'));
 app.use('/logout', require('./routes/logout'));
-app.use('/auth', require('./routes/googleAuth'));
+app.use('/auth', require('./routes/FirebaseAuth'));
 
 app.use(verifyJWT);
 app.use('/users', require('./routes/api/Users'))
