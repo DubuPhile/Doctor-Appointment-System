083072e500fec6de4870b706fd33d1b241b0baf8
Charles <enagecharles27@gmail.com>
2026-01-12 00:31:04 +0800

    styling doc card

--- client/src/components/DoctorList.jsx
+++ client/src/components/DoctorList.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react"
+import "../styles/DoctorCard.css"
 import { useNavigate } from "react-router-dom";
 
 
@@ -8,27 +8,36 @@ const DoctorList = ({ doctor }) => {
   return (
     <>
         <div 
-            className="card m-2" 
+            className="flip-card" 
             onClick={() => navigate(`/book-appointment/${doctor.userId}`)}
             style={{cursor: 'pointer'}}
         >
-            <header className="card-header text-center">
-                Dr. { doctor.firstName + " " + doctor.lastName } 
-            </header>
-            <div className="card-body">
-                <p>
-                    <b>Specialization: {doctor.specialization}</b>
-                </p>
-                <p>
-                    <b>Experience: {doctor.experience}</b>
-                </p>
-                <p>
-                    <b>Fees Per Consultation: {doctor.feesPerConsultation}</b>
-                </p>
-                <p>
-                    <b>Timings: {doctor.timings?.map(t => t.format('HH:mm')).join('-')}</b>
-                </p>
+        <div className = "flip-card-inner">
+            <div className ="flip-card-front">
+                <div>
+                    <img 
+                        src = "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+                        className = "doctorImg"
+                    />
+                </div>
+                <p className = "doctorName">Dr. { doctor.firstName + " " + doctor.lastName }</p>
+                <p><b>Schedule: {doctor.timings?.map(t => t.format('HH:mm')).join(' - ')}</b></p>
             </div>
+            <div className = "flip-card-back">
+                <div>
+                    <p>
+                        <b>Specialization: {doctor.specialization}</b>
+                    </p>
+                    <p>
+                        <b>Experience: {doctor.experience}</b>
+                    </p>
+                    <p>
+                        <b>Fees Per Consultation: {doctor.feesPerConsultation}</b>
+                    </p>
+                </div>
+            </div>
+        </div>
+            
         </div>
     </>
   )
